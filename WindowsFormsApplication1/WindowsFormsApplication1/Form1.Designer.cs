d157530c54936d81c6adc358acde873d306ea939
aagolden <aagolden@AAGOLDEN-PC01>
2016-10-10 12:56:19 -0700

    Adding first VS solution to the git project

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickMeButton = new System.Windows.Forms.Button();
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clickMeButton
+            // 
+            this.clickMeButton.Location = new System.Drawing.Point(65, 95);
+            this.clickMeButton.Name = "clickMeButton";
+            this.clickMeButton.Size = new System.Drawing.Size(75, 23);
+            this.clickMeButton.TabIndex = 0;
+            this.clickMeButton.Text = "Click Me!";
+            this.clickMeButton.UseVisualStyleBackColor = true;
+            this.clickMeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Location = new System.Drawing.Point(94, 158);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(35, 13);
+            this.mainLabel.TabIndex = 1;
+            this.mainLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.mainLabel);
+            this.Controls.Add(this.clickMeButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.Label mainLabel;
+    }
+}
+
