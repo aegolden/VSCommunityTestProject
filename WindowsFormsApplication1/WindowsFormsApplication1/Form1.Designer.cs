ee86b32f1886642d43eb105483bc987fe7e2470d
rlgolden1984 <rebeccalgolden@gmail.com>
2016-10-10 17:07:39 -0700

    Button name changes.

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -29,26 +29,26 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Farm));
-            this.clickMeButton = new System.Windows.Forms.Button();
+            this.hiveOneButton = new System.Windows.Forms.Button();
             this.mainLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.hiveTwoButton = new System.Windows.Forms.Button();
+            this.orchardButton = new System.Windows.Forms.Button();
+            this.greenhouseButton = new System.Windows.Forms.Button();
+            this.coopButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // clickMeButton
+            // hiveOneButton
             // 
-            this.clickMeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.clickMeButton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.clickMeButton.Location = new System.Drawing.Point(22, 20);
-            this.clickMeButton.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.clickMeButton.Name = "clickMeButton";
-            this.clickMeButton.Size = new System.Drawing.Size(138, 39);
-            this.clickMeButton.TabIndex = 0;
-            this.clickMeButton.Text = "Hive One";
-            this.clickMeButton.UseVisualStyleBackColor = true;
-            this.clickMeButton.Click += new System.EventHandler(this.button1_Click);
+            this.hiveOneButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hiveOneButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.hiveOneButton.Location = new System.Drawing.Point(22, 20);
+            this.hiveOneButton.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.hiveOneButton.Name = "hiveOneButton";
+            this.hiveOneButton.Size = new System.Drawing.Size(138, 39);
+            this.hiveOneButton.TabIndex = 0;
+            this.hiveOneButton.Text = "Hive One";
+            this.hiveOneButton.UseVisualStyleBackColor = true;
+            this.hiveOneButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // mainLabel
             // 
@@ -63,57 +63,57 @@
             this.mainLabel.Text = "Output...";
             this.mainLabel.Click += new System.EventHandler(this.mainLabel_Click);
             // 
-            // button1
+            // hiveTwoButton
             // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button1.Location = new System.Drawing.Point(22, 69);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 39);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Hive Two";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.hiveTwoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hiveTwoButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.hiveTwoButton.Location = new System.Drawing.Point(22, 69);
+            this.hiveTwoButton.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.hiveTwoButton.Name = "hiveTwoButton";
+            this.hiveTwoButton.Size = new System.Drawing.Size(138, 39);
+            this.hiveTwoButton.TabIndex = 2;
+            this.hiveTwoButton.Text = "Hive Two";
+            this.hiveTwoButton.UseVisualStyleBackColor = true;
+            this.hiveTwoButton.Click += new System.EventHandler(this.button1_Click_1);
             // 
-            // button2
+            // orchardButton
             // 
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(22, 118);
-            this.button2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 39);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Orchard";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.orchardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.orchardButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.orchardButton.Location = new System.Drawing.Point(22, 118);
+            this.orchardButton.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.orchardButton.Name = "orchardButton";
+            this.orchardButton.Size = new System.Drawing.Size(138, 39);
+            this.orchardButton.TabIndex = 3;
+            this.orchardButton.Text = "Orchard";
+            this.orchardButton.UseVisualStyleBackColor = true;
+            this.orchardButton.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // greenhouseButton
             // 
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button3.Location = new System.Drawing.Point(22, 168);
-            this.button3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(138, 39);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Greenhouse";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.greenhouseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.greenhouseButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.greenhouseButton.Location = new System.Drawing.Point(22, 168);
+            this.greenhouseButton.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.greenhouseButton.Name = "greenhouseButton";
+            this.greenhouseButton.Size = new System.Drawing.Size(138, 39);
+            this.greenhouseButton.TabIndex = 4;
+            this.greenhouseButton.Text = "Greenhouse";
+            this.greenhouseButton.UseVisualStyleBackColor = true;
+            this.greenhouseButton.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // coopButton
             // 
-            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button4.Location = new System.Drawing.Point(22, 217);
-            this.button4.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(138, 39);
-            this.button4.TabIndex = 5;
-            this.button4.Text = "Coop";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.coopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.coopButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.coopButton.Location = new System.Drawing.Point(22, 217);
+            this.coopButton.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.coopButton.Name = "coopButton";
+            this.coopButton.Size = new System.Drawing.Size(138, 39);
+            this.coopButton.TabIndex = 5;
+            this.coopButton.Text = "Coop";
+            this.coopButton.UseVisualStyleBackColor = true;
+            this.coopButton.Click += new System.EventHandler(this.button4_Click);
             // 
             // Farm
             // 
@@ -122,12 +122,12 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1358, 547);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.coopButton);
+            this.Controls.Add(this.greenhouseButton);
+            this.Controls.Add(this.orchardButton);
+            this.Controls.Add(this.hiveTwoButton);
             this.Controls.Add(this.mainLabel);
-            this.Controls.Add(this.clickMeButton);
+            this.Controls.Add(this.hiveOneButton);
             this.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
@@ -141,12 +141,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button clickMeButton;
+        private System.Windows.Forms.Button hiveOneButton;
         private System.Windows.Forms.Label mainLabel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button hiveTwoButton;
+        private System.Windows.Forms.Button orchardButton;
+        private System.Windows.Forms.Button greenhouseButton;
+        private System.Windows.Forms.Button coopButton;
     }
 }
 
